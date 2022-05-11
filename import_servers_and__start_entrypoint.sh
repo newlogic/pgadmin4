141c0eae675c02fddbb6925109e7e35104bf6e1c
Jérémy Bethmont <jeremy@newlogic.com>
2022-05-11 08:50:20 +0700

    Improved pgpass file

--- import_servers_and__start_entrypoint.sh
+++ import_servers_and__start_entrypoint.sh
@@ -3,15 +3,17 @@
 /entrypoint.sh &
 pid=$!
 
+envsubst < /pgadmin4/pass.pgpass.tmpl > /tmp/pass.pgpass
+chmod 0600 /tmp/pass.pgpass
+
 until [ -f /var/lib/pgadmin/pgadmin4.db ]
 do
      sleep 1
 done
 
 if /venv/bin/python3 setup.py --dump-servers /dev/null | grep "Configuration for 0 servers dumped"; then
-    envsubst < /pgadmin4/pass.pgpass.tmpl > /tmp/pass.pgpass
-    chmod 0600 /tmp/pass.pgpass
     envsubst < /pgadmin4/servers.json.tmpl > /tmp/servers.json
     /venv/bin/python3 setup.py --load-servers /tmp/servers.json
 fi
+
 wait $pid
